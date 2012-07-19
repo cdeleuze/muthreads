54fed153d9f495321011f0f645a207f476e13e30
Christophe Deleuze <christophe@esisar>
2012-07-19 13:20:04 +0200

    Clean up of muthr.ml

--- muthr-0.5/muthr.ml
+++ muthr-0.5/muthr.ml
@@ -11,7 +11,7 @@
    - OK - race condition on read_or_take and others
           the mv.read <- None should be done immediately in check_io
 
-   - really_write_or_take : quelle sémantique ?
+   - really_write_or_take : what semantics?
 
    - nb of threads active
    - ...
@@ -49,8 +49,8 @@ let dbg_xfd =
   else fun xfd -> ()
 
 (* A thread is made of a continuation, a context stack, and a
-   [pending_io ref], used to clean-up IO data structures if the thread
-   is cancelled (eg from a timeout) while blocked on an IO
+   [pending_io ref], used to clean-up I/O data structures if the
+   thread is cancelled (eg from a timeout) while blocked on an I/O
    operation. *)
 
 type pending_io =
@@ -74,17 +74,26 @@ and context = ctxt_elt list
 let ctxt = ref []
 let io = ref (ref Nofd)
 
-(* [runq] is a FIFO of currently runnable threads.  [enqueue k] adds
-   the continuation [k] of the current thread in [runq].  [transfer]
-   adds a thread in [runq].
+(* [runq] is a FIFO of currently runnable threads.  [enqueue] and
+   [dequeue] add/get a thread in/from [runq].  [continue k] adds the
+   continuation [k] of the current thread in [runq], using the current
+   context stack and pending io (from globals [ctxt] and [io]).
+   [context] does the same thing but uses the provided context.
 
-   [term] is set to false by suspending (yielding/blocking/sleeping)
-   threads. *)
+   [continue] is to be run from an operation called by the thread
+   itself, while enqueue should be called from the scheduler (or
+   another thread).
+
+   [term] is used by the scheduler.
+ *)
 
 let runq = Queue.create ()
-let enqueue  k = Queue.push (!io, !ctxt, k) runq
-let transfer p = Queue.push p runq
+let enqueue p  = Queue.push p runq
 let dequeue () = Queue.take runq
+
+let continue  k = Queue.push (!io, !ctxt, k) runq
+let context c k = Queue.push (!io, c , k) runq
+
 let term = ref true
 
 exception Stop
@@ -103,8 +112,8 @@ let rec put_mvar out v k =
       out.write <- Some ((!io,!ctxt,k),v)
 
   | { v=None; read=Some(l,c,r); write=None } -> 
-      out.read <- None; enqueue k; out.v <- Some v;
-      transfer (l,c,(fun () -> take_mvar out r));
+      out.read <- None; continue k; out.v <- Some v;
+      enqueue (l,c,(fun () -> take_mvar out r));
       term := false
 
   | { v=None; read=None; write=None } -> out.v <- Some v; k ()
@@ -117,7 +126,7 @@ and take_mvar inp k =
 
   | { v=Some v; read=None; write=Some((l,c,r),v') } -> 
       inp.v <- None; inp.write <- None;
-      transfer (l,c,(fun () -> put_mvar inp v' r)); k v
+      enqueue (l,c,(fun () -> put_mvar inp v' r)); k v
 
   | { v=None; read=None; write=_ } -> inp.read <- Some(!io, !ctxt,k);
       term := false
@@ -145,7 +154,7 @@ let take_fifo f k =
 let put_fifo f v =
   Queue.add v f.q;
   match f.w with
-  | Some(l,c,r) -> transfer (l,c, (fun () -> take_fifo f r)); f.w <- None
+  | Some(l,c,r) -> enqueue (l,c, (fun () -> take_fifo f r)); f.w <- None
   | None -> ()
 
 
@@ -172,14 +181,13 @@ let insert_sleeper date p =
    for next wakeup, or -1 if no more sleepers. *)
 
 let enqueue_expired_sleepers () =
-  let now = Unix.gettimeofday ()
-  in
+  let now = Unix.gettimeofday () in
   let rec loop l =
     match l with
       | [] -> []
       | (t,p)::rest ->
 	if t<=now then begin
-	  transfer p;
+	  enqueue p;
 	  loop rest
 	end else l
   in
@@ -227,73 +235,73 @@ let rem_from_xfd xfd fd =
 (* A read operation on [fd] was blocked, we know it's now possible, so
    do it.  Precisely:
    \begin{itemize}
-   \item [transfer] a thread doing the operation and passing the result
+   \setlength{\itemsep}{0cm}
+   \item [enqueue] a thread doing the operation and passing the result
      to the continuation,
-   \item reset the thread [pending_io] (since we're no more blocked on
-     the IO operation),
+   \item reset the thread [pending_io] (since it's no more blocked on
+     the I/O operation),
    \item reset the [readers] entry.
    \end{itemize}
 
-   The [go] parameter is for combined IO/MVar operations, see relevant
-   section.
-  *)
+   The [go] parameter is for combined I/O--MVar operations, if it is
+   set the [read] operation is cancelled, see relevant section.  *)
 
 let do_read_fd fd =
-  match readers.(Obj.magic fd) with
+  let op = readers.(Obj.magic fd) in
+  readers.(Obj.magic fd) <- NoRead;
+  match op with
     | Read(go, s, ofs, len, (r,c,k)) -> 
+      r := Nofd;
       if !go then begin
 	go := false;
-	transfer (r,c,(fun () -> k (Unix.read fd s ofs len)))
-      end;
-      r := Nofd;
-      readers.(Obj.magic fd) <- NoRead
-	
+	enqueue (r,c,(fun () -> k (Unix.read fd s ofs len)))
+      end
+
     | Recv(s, ofs, len, l, (r,c,k)) -> failwith "recv not implemented"
       
     | Recvfrom(s, ofs, len, l, (r,c,k)) -> 
-      transfer (r, c,(fun () -> k (Unix.recvfrom fd s ofs len l)));
       r := Nofd;
-      readers.(Obj.magic fd) <- NoRead
+      enqueue (r, c,(fun () -> k (Unix.recvfrom fd s ofs len l)))
 	
     | Accept(go,(r,c,k)) -> 
+      r := Nofd;
       if !go then begin
 	go := false;
-	transfer (r, c, (fun () -> 
+	enqueue (r, c, (fun () -> 
 	  let (fd',addr) = Unix.accept fd in
 	  Unix.set_nonblock fd'; k (fd', addr)))
-      end;
-      r := Nofd;
-      readers.(Obj.magic fd) <- NoRead
+      end
 	
     | NoRead -> error "lost rfd"
 
 (* The same thing for a write operation. *)
 
 let do_write_fd fd =
-  match writers.(Obj.magic fd) with
+  let op = writers.(Obj.magic fd) in
+  writers.(Obj.magic fd) <- NoWrite;
+  match op with
     | Write(go, s, ofs, len, (r,c,k)) ->
+      r := Nofd;
       if !go then begin
 	go := false;
-	transfer (r, c, (fun () -> k (Unix.write fd s ofs len)))
-      end;
-      r := Nofd;
-      writers.(Obj.magic fd) <- NoWrite
+	enqueue (r, c, (fun () -> k (Unix.write fd s ofs len)))
+      end
 	
     | Connect(go,(r,c,k)) -> 
+      r := Nofd;
       if !go then begin
 	go := false;
 	let err = Unix.getsockopt_int fd Unix.SO_ERROR 
 	in
 	if err = 0 then
-	  transfer (r,c,k)
+	  enqueue (r,c,k)
 	(* ZZZ + EINTR ? *)
 	else 
-	      (* connect has failed, we raise an exception *in* the thread *)
-	  transfer (r,c, fun () -> 
+	  (* [connect] has failed, we raise an exception \textbf{in} the
+	     thread. *)
+	  enqueue (r,c, fun () -> 
 	    raise (Unix.Unix_error(Obj.magic err, "connect", "")))
-      end;
-      r := Nofd;
-      writers.(Obj.magic fd) <- NoWrite
+      end
 	
     | NoWrite -> error "lost wfd"
 
@@ -310,7 +318,7 @@ let do_write_fd fd =
    perform the operations.  Note that it doesn't need to set term to
    [false], since it's always running in an empty context.
 
- *)
+*)
 
 let rec check_io () =
   debug "check_io";
@@ -329,7 +337,7 @@ let rec check_io () =
     List.iter do_read_fd  rdo;
     List.iter do_write_fd wdo
   end;
-  enqueue check_io
+  continue check_io
 ;;
 
 (*s Exception handling *)
@@ -337,14 +345,17 @@ let rec check_io () =
 (* A thread has raised an exception [e]: try to catch it, unrolling
    the context stack [cs] until [e] is catched. 
 
-   When a [TryWith(h,k)] context element is found, we schedule the
-   handler to run, continued by k ([Seq] context element). If the
-   handler refuses the exception (it raises [Match_failure] during the
-   first step) we make it call [trycatch] with the initial exception
+   When a [TryWith(h,k)] context element is found, we wrap the handler
+   [h] in [h'] and schedule it to run, followed by k ([Seq] context
+   element).  The wrapping is to check if the handler refuses the
+   exception (it raises [Match_failure] during the first step): in
+   this case, we make it call [trycatch] with the initial exception
    (thus looking for another exception handler deeper in the stack).
    If, during this first step, it raises another exception, just pass
-   it on.  Exceptions raised in the next steps of the handler will be
-   handled normally.
+   it on (it has accepted the provided exception but then raised one).
+
+   Exceptions raised in the next steps of the handler will be
+   processed normally. ZZZ
 
 *)
 
@@ -352,17 +363,17 @@ let rec trycatch e cs lio =
   debug "trycatch";
   match cs with
 
-  | TryWith(h,f')::t -> 
-    let k () =
+  | TryWith(h,k)::t -> 
+    let h' () =
       debug "trycatch: running handler"; 
-      (try h e with 
+      (try h e with
 	| Match_failure _ -> debug "trycatch: uncaught"; trycatch e !ctxt !io
 	| other -> raise other);
       debug "trycatch: handler done (first step)"
     in
     debug "found TryWith context";
     term := false;
-    transfer (lio, Seq(f')::t, k)
+    enqueue (lio, Seq(k)::t, h')
 	
   | Timeout(s,_,_) :: t -> s := true; trycatch e t lio    (* kill timer *)
   | NoTimeout _    :: t -> trycatch e t lio
@@ -379,17 +390,13 @@ let rec trycatch e cs lio =
 
 (*s Scheduler *)
 
-(* run function f, catching exceptions as specified by c *)
+(* Set up context and io, then run thread (run function [f], catching
+   exceptions as specified by context [c]). *)
 
-let run f c io =
-  try f () with e -> trycatch e c io (* raise e *)
-
-(* set up context and io, then run thread *)
-
-let set_and_run (lio, c, f) =
+let run (lio, c, f) =
   io := lio;
   ctxt := c;
-  run f c lio
+  try f () with e -> trycatch e c lio  (* raise e *)
 
 let rec find_shot cs =
   match cs with
@@ -407,12 +414,20 @@ let dbg_ctxt c =
 			       | TryWith _   -> "W") c)))
 
 (* The scheduler is an infinite loop.  Dequeue a thread, check if it's
-   not been shot, [run] it, enqueue continuation if applicable.  *)
+   not been shot, [run] it, enqueue continuation with updated context
+   if applicable. 
+
+   [term] is set to true before calling the thread current function.
+   The thread will set it to false if it doesn't want the scheduler to
+   schedule it again (either it dies or has taken care of its
+   continuation itself).
+
+  *)
 
 let sched () =
-  let pops_enqueue msg c t = debug msg; ctxt:=c; enqueue t
+  let context_msg msg c t = debug msg; context c t
   in
-  enqueue check_io;
+  enqueue (ref Nofd, [], check_io);
   try
     while true do
       term := true;
@@ -424,52 +439,35 @@ let sched () =
 	| Timeout(shot, date, t) :: c' ->
 	  if !shot then debug "thread shot" else begin (* useless ! find shot ZZZ *)
 	    debug "thread not shot";
-	    set_and_run p;
-	    if !term then begin  (* timed operation terminated:   *)
-	      shot := true;      (* - kill timer         *)
-	      pops_enqueue "killing timer" c' t
+	    run p;
+	    if !term then begin  (* timed operation completed:   *)
+	      shot := true;      (* kill timer         *)
+	      context_msg "killing timer" c' t
 	    end
 	  end
 	      
-	| NoTimeout(t)::c' -> set_and_run p;
-	    if !term then pops_enqueue "" c' t
+	| NoTimeout(t)::c' -> run p;
+	    if !term then context c' t
 
-	| Seq(t)::c' -> set_and_run p;
-	    if !term then pops_enqueue "end Seq" c' t
+	| Seq(t)::c' -> run p;
+	    if !term then context_msg "end Seq" c' t
 
-	| [] -> set_and_run p
+	| [] -> run p
 
-	| TryWith(h,t)::c' -> debug "run trywith"; set_and_run p;
-	    if !term then pops_enqueue "trywith done!" c' t
+	| TryWith(h,t)::c' -> debug "run trywith"; run p;
+	    if !term then context_msg "trywith done!" c' t
       end;
       flush stdout
     done
   with Stop -> () (* ZZZ empty runq and other data structs? *)
 
 
-(* A new timeout is valid except if an already established timeout
-   would expire sooner.  In this case we will ignore the [timeout]
-   instruction (but need to push a [NoTimeout] context element to
-   store the given continuation).
-
-   The already established timeout is the innermost by definition. 
-   ZZZ ???*)
-
-let is_valid_timeout d =
-  let rec loop c =
-    match c with
-    | Timeout(_, d',_)::_ -> d<d'
-    | [] -> true
-    | h::t -> loop t
-  in
-  loop !ctxt
-
 
 (*s Basic API *)
 
 let skip  k = k ()
-let yield k = term := false; enqueue k
-let spawn k = debug "spawn"; transfer (ref Nofd, (*!ctxt*) [], k)
+let yield k = term := false; continue k
+let spawn k = debug "spawn"; enqueue (ref Nofd, (*!ctxt*) [], k)
 let start () = sched ()
 let stop  () = raise Stop
 
@@ -487,8 +485,8 @@ let merge t1 t2 after =
     if !you_re_last then after ()
     else you_re_last := true
   in
-  enqueue (fun () -> t1 >>= finish);
-  enqueue (fun () -> t2 >>= finish)
+  continue (fun () -> t1 >>= finish); (*ZZZ*)
+  continue (fun () -> t2 >>= finish)
 
 
 let sleep t k =
@@ -505,13 +503,23 @@ let sleep_until d k =
 
 (*s Exception handling *)
 
-(* run f, if exception is raised, give it to h.  In any case, finally
-   go on with after *)
+(* We want to run [f], with [h] as exception handler.  When this is
+done (either [f] has completed or [h] has caught an exception, execute
+[after].
+
+We simply schedule [f] to be executed in a context on which has been
+pushed a exception handling frame ([TryWith] context element).
+*)
 
 let trywith f h after =
   debug "entering trywith";
   term := false;
-  ctxt := TryWith(h, after) :: !ctxt; enqueue f
+  context (TryWith(h, after)::!ctxt) f
+
+(*
+
+
+*)
 
 let trywithk f h after =
   debug "entering trywithk";
@@ -522,13 +530,15 @@ let trywithk f h after =
   let h e = h (fun r -> debug "trywithk: h setting res"; res := Some r) e in
   let th  = f (fun r -> debug "trywithk: f setting res"; res := Some r)
   in
-  ctxt := TryWith(h, k) :: !ctxt; enqueue th
+  context (TryWith(h, k) :: !ctxt) th
 
 
 (*s Timeouts *)
+
 (* This is to be called with pending io of a thread when a pending I/O
-   operation is aborted -- this can only occur on timeouts. ZZZ
-   trywith? *)
+   operation is aborted -- this can only occur on timeouts (more
+   generally it is needed if the thread may be killed by another
+   one). *)
 
 let clear_pending_io io =
   match !io with
@@ -536,9 +546,36 @@ let clear_pending_io io =
     | FdRead  fd -> readers.(Obj.magic fd) <- NoRead;  rem_from_xfd rfd fd
     | FdWrite fd -> writers.(Obj.magic fd) <- NoWrite; rem_from_xfd wfd fd
 
+(* A new timeout is valid except if an already established timeout
+   would expire sooner.  The shorter already established timeout is
+   the syntactically innermost (ie the most recently pushed on the
+   stack) by construction.  *)
 
+let is_valid_timeout d =
+  let rec loop c =
+    match c with
+      | Timeout(_, d',_)::_ -> d<d'
+      | [] -> true
+      | h::t -> loop t
+  in
+  loop !ctxt
 
-let timeout t f h f' =
+(* A timeout is implemented by spawning a thread sleeping for the
+   specified amount of time, sharing a boolean ref with the initial
+   thread.  The first one to complete will set the boolean ref to
+   true, thus informing the other one it has ``shot'' first.
+
+   If the timer shots first, it has to [clear_pending_io] in case the
+   timed thread was blocked on an I/O operation.
+
+   If the timeout is not valid, we don't actually have to set a timer,
+   but we need to arrange for the provided continuation to run when
+   the operation has completed: for this we push a [NoTimeout] context
+   element to the stack.
+
+*)
+
+let timeout t f h k =
   let shot = ref false in
   let t = Unix.gettimeofday () +. t in
   let tio = !io
@@ -546,28 +583,27 @@ let timeout t f h f' =
   term := false;
   if is_valid_timeout t then begin 
     debug "set timeout";
-    transfer (tio, Timeout(shot,t,f')::!ctxt, f);
-    (* the timer kills the thread when firing *)
+    enqueue (tio, Timeout(shot,t,k)::!ctxt, f);
     let timer () = 
       if !shot then (term := false; debug "old timer") else begin
 	debug "timer fire!";
-	shot := true;
+	shot := true;          (* kill the thread *)
 	clear_pending_io tio;
 	h ()
       end
     in
     insert_sleeper t (ref Nofd, !ctxt, timer)
-  end else (debug "notimeout!";
-	    transfer (!io, NoTimeout(f')::!ctxt, f))
+  end else
+    (debug "notimeout!";
+     enqueue (!io, NoTimeout(k)::!ctxt, f))
 
 (* timeout with passing of result ... *)
-(* ok, details of formulation for user should be thought about *)
+(* ok, but can we make a cleaner interface? *)
 
 let timeoutk t f h f' =
   let shot = ref false in
   let t = Unix.gettimeofday () +. t in
-  let tio = !io
-  in
+  let tio = !io in
   let res = ref None
   in
   term := false;
@@ -576,22 +612,22 @@ let timeoutk t f h f' =
   in
   if is_valid_timeout t then begin 
     debug "set timeoutk";
-    transfer (tio, Timeout(shot,t,k)::!ctxt, th);
-    (* the timer kills the thread when firing *)
+    enqueue (tio, Timeout(shot,t,k)::!ctxt, th);
     let timer () = 
       if !shot then (term := false; debug "old timerk") else begin
 	debug "timerk fire!";
-	shot := true;
+	shot := true;           (* kill the thread *)
 	clear_pending_io tio;
 	h ()
       end
     in
     insert_sleeper t (ref Nofd, !ctxt, timer)
-  end else (debug "notimeout!";
-	    transfer (!io, NoTimeout(k)::!ctxt, th))
+  end else
+  (debug "notimeoutk!";
+   enqueue (!io, NoTimeout(k)::!ctxt, th))
 
 
-(*s Temporised operations on MVars *)
+(*s Temporized operations on MVars *)
 
 let timed_take d mv k =
   timeoutk d
@@ -605,7 +641,7 @@ let timed_put d mv v k =
     (fun () -> k (Some v))
     (fun () -> k None)
 
-(*s non blocking I/O  -- low-level API *)
+(*s Non blocking I/O  -- low-level API *)
 
 (* [readb] is used both for simple [read] and [read_or_take].
 
@@ -635,14 +671,14 @@ let readb go fd s ofs len k =
     with Unix.Unix_error((Unix.EAGAIN | Unix.EWOULDBLOCK | Unix.EINTR), _, _)
       -> 0, false
   in if ok then k res
-  else i*)begin
-    debug "readb: blocked";
-    (* if List.mem fd !rfd then debug "fd already in rfd!" else*)
-    rfd := fd :: !rfd;
-    !io := FdRead fd;
-    readers.(Obj.magic fd) <- Read (go, s, ofs, len, (!io, !ctxt, k));
-    term := false
-  end
+  else begin i*)
+  debug "readb: blocked";
+  (* if List.mem fd !rfd then debug "fd already in rfd!" else*)
+  rfd := fd :: !rfd;
+  !io := FdRead fd;
+  readers.(Obj.magic fd) <- Read (go, s, ofs, len, (!io, !ctxt, k));
+  term := false
+(*i  end i*)
 
 let read fd s ofs len k =
   readb (ref true) fd s ofs len k
@@ -660,7 +696,7 @@ let read_or_take mv fd s ofs len kread ktake =
 	  go := false;
 	  rem_from_xfd rfd fd;
 	  readers.(Obj.magic fd) <- NoRead;
-	  enqueue (fun () -> ktake v)
+	  continue (fun () -> ktake v)
 	end else (* read has already been scheduled, cancel ourselves *)
 	  mv.v <- Some v;
 	terminate ()
@@ -668,7 +704,7 @@ let read_or_take mv fd s ofs len kread ktake =
       mv.read <- Some(tio, c, mvar_wait);
       readb go fd s ofs len (fun l -> mv.read <- None; kread l)
 	
-(* The same applies for the other I/O primitives *)
+(* The same applies for the other I/O primitives. *)
 
 let writeb go fd s ofs len k =
   let res, ok =
@@ -700,7 +736,7 @@ let write_or_take mv fd s ofs len kwrite ktake =
 	if !go then begin
 	  rem_from_xfd wfd fd;
 	  writers.(Obj.magic fd) <- NoWrite;
-	  enqueue (fun () -> ktake v)
+	  continue (fun () -> ktake v)
 	end else
 	  mv.v <- Some v;
 	terminate ();
@@ -763,7 +799,7 @@ let accept_or_take mv fd kaccept ktake =
 	if !go then begin
 	  rem_from_xfd rfd fd;
 	  readers.(Obj.magic fd) <- NoRead;
-	  enqueue (fun () -> ktake v)
+	  continue (fun () -> ktake v)
 	end else
 	  mv.v <- Some v;
 	terminate ()
@@ -801,7 +837,7 @@ let connect_or_take mv fd addr kconnect ktake =
 	if !go then begin
 	  rem_from_xfd wfd fd;
 	  writers.(Obj.magic fd) <- NoWrite;
-	  enqueue (fun () -> ktake v)
+	  continue (fun () -> ktake v)
 	end else
 	  mv.v <- Some v;
 	terminate ()
@@ -842,7 +878,6 @@ let recvfrom fd s ofs len l k =
 
 (*  
    ZZZ if we protect the Unix.close in a try/with, the select then fails...
-   à cause du [pending_io] qui n'était pas géré ??? apparemment non
  *)
 
 let close fd =
